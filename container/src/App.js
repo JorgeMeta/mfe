4081ddc3b58acd81003b0c4cb9fc81970dcb01fd
Jorge <jorge.attie@meta.com.br>
2025-09-23 09:48:25 -0300

    classeName

--- container/src/App.js
+++ container/src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import MarketingRectComponent from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingRectComponent />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <>
+          <Header />
+          <MarketingRectComponent />
+        </>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
